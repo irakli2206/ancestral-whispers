a063757990d927dc05a1fb35e86881febfd5d832
irakli2206 <iraklibego1@gmail.com>
2024-06-15 12:52:56 +0400

    banner

---
+++ src/components/banner.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Banner = () => {
+    return (
+        <div className="relative  bg-background-black border-b z-[9999] px-6 py-2.5 sm:px-3.5 ">
+
+            <div className="flex flex-wrap w-full justify-center items-center ">
+                <p className="text-sm leading-6 text-primary flex items-center">
+                    <strong className="font-semibold">Site is being rebuilt</strong>
+                    <svg viewBox="0 0 2 2" className="mx-2 h-0.5 w-0.5 fill-current hidden md:inline" aria-hidden="true">
+                        <circle cx={1} cy={1} r={1} />
+                    </svg>
+                    <p className="hidden md:block">The website's interface is receiving a massive update, bugs are a possibility</p>
+                </p>
+
+            </div>
+            <div className="flex flex-1 justify-end">
+                <button type="button" className="-m-3 p-3 focus-visible:outline-offset-[-4px]">
+                    <span className="sr-only">Dismiss</span>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
--- src/layouts/root.tsx
+++ src/layouts/root.tsx
@@ -1,3 +1,4 @@
+import Banner from '@/components/banner'
 import Footer from '@/components/footer'
 import Navbar from '@/components/navbar'
 import React from 'react'
@@ -6,6 +7,7 @@ import { Outlet } from 'react-router-dom'
 const Root = () => {
     return (
         <>
+            <Banner />
             <Navbar />
 
             <Outlet />
